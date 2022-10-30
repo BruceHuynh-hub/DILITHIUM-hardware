820bcb6e71436ae5a61341b3d9f5893c7e627300
Matthew Wong <matt11wong@gmail.com>
2022-10-30 10:12:56 -0400

    Update sipo.v

--- rtl/sipo.v
+++ rtl/sipo.v
@@ -12,36 +12,32 @@ module sipo(
     output [( n - 1 ):0] output;
     localparam regamount  = ( n / m );
     genvar i;
-    reg [0:( ( n / m ) - 1 )]reg;
+    reg [0:((n/m) - 1 )]reg;
     generate
-        for ( i = ( ( n / m ) - 1 ) ; ( i >= 0 ) ; i = ( i - 1 ) )
+        for ( i =((n/m)-1) ; (i>= 0) ; i = (i-1))
         begin : output_gen
-            assign output[( ( m * i ) + ( m - 1 ) ):( m * i )] = reg[( ( n / m ) - 1 )];
+            assign output[((m * i) + (m - 1)):(m * i)] = reg[( (n/m)-1)];
         end
     endgenerate
     generate
-        for ( i = ( ( n / m ) - 2 ) ; ( i >= 0 ) ; i = ( i - 1 ) )
+        for (i = ((n/m)-2); (i>= 0); i = (i-1))
         begin : regX_gen
-            always @ (  posedge clk)
+            always @ ( posedge clk)
             begin : regX
                 if ( clk == 1'b1 ) 
                 begin
                     if ( en == 1'b1 ) 
-                    begin
-                        reg[i] <= reg[( i + 1 )];
-                    end
+                        reg[i] <= reg[(i + 1)];
                 end
             end
         end
     endgenerate
-    always @ (  posedge clk)
+    always @ ( posedge clk)
     begin : regLast
         if ( clk == 1'b1 ) 
         begin
             if ( en == 1'b1 ) 
-            begin
-                reg[( ( n / m ) - 1 )] <= input;
-            end
+                reg[(( n / m ) - 1 )] <= input;
         end
     end
 endmodule 
