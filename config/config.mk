8fe27d961f173bc82a9f95635e4192ecd1c178b8
Bruce Huynh <78281552+BruceHuynh-hub@users.noreply.github.com>
2022-10-30 13:04:12 -0400

    config folder

---
+++ config/config.mk
@@ -0,0 +1,9 @@
+export DESIGN_NAME = mymult1
+export PLATFORM    = sky130hd
+
+export VERILOG_FILES = /OpenROAD-flow-scripts/flow/mydesign/lab1/m1/rtl/mymult1.v
+export SDC_FILE      = /OpenROAD-flow-scripts/flow/mydesign/lab1/m1/config/constraint.sdc
+
+# These values must be multiples of placement site
+export DIE_AREA    =  0    0 180 180
+export CORE_AREA   =  10  10 170 170
