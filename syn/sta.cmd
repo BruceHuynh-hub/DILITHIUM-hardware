286ee645bec4d6326978637d08be6be084df3e8a
Bruce <brucehuynh67@gmail.com>
2022-11-10 01:31:46 -0500

    scripts added for area and speed opt

--- syn/sta.cmd
+++ syn/sta.cmd
@@ -1,6 +1,6 @@
 read_liberty /mnt/volume_nyc1_01/skywater-pdk/libraries/sky130_fd_sc_hd/latest/timing/sky130_fd_sc_hd__tt_025C_1v80.lib
-read_verilog mymult1_gl.v
-link_design mymult1
+read_verilog decomposer_gl.v
+link_design decomposer_unit
 create_clock -name clk -period 10 {clk}
 set_input_delay -clock clk 0 {reset a b start clk}
 report_checks
