42e5ff88829ccb7eeb122b2bb1ef9a1b89f482ce
Bruce <brucehuynh67@gmail.com>
2022-12-11 12:36:35 -0500

    decomp config

--- decomp/config/config.mk
+++ decomp/config/config.mk
@@ -5,8 +5,10 @@ export VERILOG_FILES = /OpenROAD-flow-scripts/flow/mydesign/decomp/decomp/decomp
 /OpenROAD-flow-scripts/flow/mydesign/decomp/decomp/decomp_map1.v \
 /OpenROAD-flow-scripts/flow/mydesign/decomp/decomp/coeff_decomposer.v 
 
+export ABC_CLOCK_PERIOD_IN_PS = 10000
+
 export SDC_FILE      = /OpenROAD-flow-scripts/flow/mydesign/decomp/config/constraint.sdc
 
 # These values must be multiples of placement site
-export DIE_AREA    =  0  0 180 180
-export CORE_AREA   =  0  0 180 180
+export DIE_AREA    =  0  0 350 350
+export CORE_AREA   =  0  0 350 350
--- decomp/config/constraint.sdc
+++ decomp/config/constraint.sdc
@@ -1,12 +1,11 @@
-current_design gcd
+current_design decomposer_unit
 
 set clk_name  core_clock
 set clk_port_name clk
-set clk_period 10
+set clk_period 10000
 set clk_io_pct 0.2
 
 set clk_port [get_ports $clk_port_name]
-
 create_clock -name $clk_name -period $clk_period $clk_port
 
 set non_clock_inputs [lsearch -inline -all -not -exact [all_inputs] $clk_port]
